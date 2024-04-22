b3029469d2e9c9d67d4a016be4d692b47e1a7779
Lucas Silvestre <88158275+LSilvestreD3v@users.noreply.github.com>
2024-04-22 16:34:53 +0100

    delete all
    falha nos arquivos

--- Numero_Game-FinalizadO/Primeira-Logica/apptreino.js
+++
@@ -1,6 +0,0 @@
-let nome = Lucas
-let sobrenome = Silvestre
-let pergunta = prompt('qual seu nome');
-let pergunta2 = prompt('qual seu sobrenome');
-let nomeCompleto = nome + sobrenome;
-console.log(nomeCompleto);
